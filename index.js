d5dbba8a6369d26aa3046e1732cee1dca0c97176
yebecyedek <yusufberkcekic11@gmail.com>
2024-11-27 16:17:43 +0300

    Fix blocking global shortcuts on all windows

--- index.js
+++ index.js
@@ -45,35 +45,39 @@ ipcMain.on('gesture-change', (event, data) => {
 });
 
 app.on("ready", () => {
-    const shortcuts = {
-        "CommandOrControl+W": 'close-tab',
-        "CommandOrControl+T": 'new-tab',
-        "CommandOrControl+Left": 'go-back',
-        "CommandOrControl+Right": 'go-forward',
-        "CommandOrControl+Option+I": 'inspect',
-        "CommandOrControl+R": 'reload-selected-page',
-        "CommandOrControl+S": 'toggle-is-vert-or-horiz',
-        "CommandOrControl+1": 'select-tab-1',
-        "CommandOrControl+2": 'select-tab-2',
-        "CommandOrControl+3": 'select-tab-3',
-        "CommandOrControl+4": 'select-tab-4',
-        "CommandOrControl+5": 'select-tab-5',
-        "CommandOrControl+6": 'select-tab-6',
-        "CommandOrControl+7": 'select-tab-7',
-        "CommandOrControl+8": 'select-tab-8',
-        "CommandOrControl+9": 'select-tab-9',
-        "CommandOrControl+0": 'select-tab-10',
-        "CommandOrControl+=": 'unselect-all-tabs'
-    };
-
-    for (const [key, message] of Object.entries(shortcuts)) {
-        globalShortcut.register(key, () => {
-            const focusedWindow = BrowserWindow.getFocusedWindow();
-            if (focusedWindow) {
-                focusedWindow.webContents.send(message);
-            }
-        });
-    }
+    app.on('browser-window-focus', () => {
+        const shortcuts = {
+            "CommandOrControl+W": 'close-tab',
+            "CommandOrControl+T": 'new-tab',
+            "CommandOrControl+Left": 'go-back',
+            "CommandOrControl+Right": 'go-forward',
+            "CommandOrControl+Option+I": 'inspect',
+            "CommandOrControl+R": 'reload-selected-page',
+            "CommandOrControl+S": 'toggle-is-vert-or-horiz',
+            "CommandOrControl+1": 'select-tab-1',
+            "CommandOrControl+2": 'select-tab-2',
+            "CommandOrControl+3": 'select-tab-3',
+            "CommandOrControl+4": 'select-tab-4',
+            "CommandOrControl+5": 'select-tab-5',
+            "CommandOrControl+6": 'select-tab-6',
+            "CommandOrControl+7": 'select-tab-7',
+            "CommandOrControl+8": 'select-tab-8',
+            "CommandOrControl+9": 'select-tab-9',
+            "CommandOrControl+0": 'select-tab-10',
+            "CommandOrControl+=": 'unselect-all-tabs'
+        };
+    app.on('browser-window-blur', () => {
+        globalShortcut.unregisterAll();
+    });
+        for (const [key, message] of Object.entries(shortcuts)) {
+            globalShortcut.register(key, () => {
+                const focusedWindow = BrowserWindow.getFocusedWindow();
+                if (focusedWindow) {
+                    focusedWindow.webContents.send(message);
+                }
+            });
+        }
+    });
 
     ipcMain.on('swipe-coordinates', (event, arg) => {
         console.log(`Swipe coordinates: (${arg[0]}, ${arg[1]})`);
